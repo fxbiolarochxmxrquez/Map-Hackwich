91b7825dee10962b3ee3d1ea109e251f4b2e3468
Fabiola Rocha Marquez <frochamarquez@bsd220.org>
2024-02-11 14:00:55 -0600

    Initial Commit

---
+++ Map Hackwich/Map_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Map_HackwichApp.swift
+//  Map Hackwich
+//
+//  Created by Fabiola Rocha Marquez on 2/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Map_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
